3a120341cd495e05280c30b4bd789ef6202334d4
wei1224hf <wei1224hf@gmail.com>
2014-01-26 20:05:42 +0800

    oiiii

---
+++ sql/drop.sql
@@ -0,0 +1,54 @@
+drop table basic_parameter ;
+ 
+ 
+ drop table basic_user ;
+ 
+
+ 
+ drop table basic_user_session ;
+ 
+
+ 
+ drop table basic_memory ;
+ 
+ drop table basic_group ;
+ 
+
+ 
+ drop table basic_group_2_user ;
+ 
+ drop table basic_permission ;
+ 
+
+ 
+ drop table basic_group_2_permission ;
+ 
+ drop table basic_node ;
+ drop table exam_subject ;
+
+ 
+ 
+ drop table exam_subject_2_group ;
+
+ drop table exam_subject_2_user_log ;
+ 
+
+ 
+ drop table exam_paper;
+ 
+
+ 
+ drop table exam_paper_log ;
+ 
+
+ 
+ drop table exam_question ;
+ 
+
+ 
+ drop table exam_question_log ;
+ 
+ drop table exam_question_log_wrongs ;
+
+ 
+ drop table exam_paper_multionline ;
