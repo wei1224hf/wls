b48c99954381a4a55c0a9802e6a81fc2813b786d
wei1224hf <wei1224hf@gmail.com>
2014-03-27 13:38:51 +0800

    yyy

--- README.md
+++
@@ -1,6 +0,0 @@
-wls
-===
-
-We Like Study!
-最新版本下载地址在 http://pan.baidu.com/s/19qYW2
-
--- install_chmod.sh
+++
@@ -1,7 +0,0 @@
-chmod 777 sql/data.sql
-chmod 777 sql/sql.sq
-chmod 777 config.xml.php
-chmod 777 file/upload/
-chmod 777 file/upload/paper/
-chmod 777 file/upload/mp3/
-chmod 777 file/upload/photo/
